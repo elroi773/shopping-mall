2f8c9b699473881f7a6d5f62879c07c0aba199ff
elroi773 <s2457@e-mirim.hs.kr>
2025-05-30 22:39:30 +0900

    index

--- index.php
+++ index.php
@@ -9,38 +9,29 @@
     <h1>상품 목록</h1>
 
     <?php 
-        // $products = [
-        //     ['id'=>1, 'name'=>'상품1', 'price' =>10000],
-        //     ['id'=>2,'name'=>'상품2', 'price' =>20000],
-        //     ['id'=>3,'name'=>'상품3', 'price' =>30000]
-        // ]; //연관배열 -> DB 연결로 바꿀 것임 
-
-        include 'db.php'; //import 비슷비슷
+        include 'db.php'; //import 비슷비슷 //DB 연결에 관련한 설정을 관련한 파일 MYSQL 연결 설정이 들어감 
         $stmt = $pdo->query("SELECT * FROM products"); //프로덕트 싹! 다! 데리고 옴
+        //PDO 는 객체임 
         $products = $stmt -> fetchAll(PDO::FETCH_ASSOC); //결과를 배열로 가지고 옴 그래서 products[0] 이렇게 할 수 있두앙두앙
+        //fetch all 를 연관 배열 현태로 넣는 명령어 pdo :: fetch _Assoc 칼럼명을 배열의 키 값으로 가지고 옴 
 
         //연관 배열 이차원배열 그대로 table에 넣은 것임  
-
-        // foreach ($products as $product) {
-        //     echo "<div>";
-        //     echo "<h2>".$product['name']."</h2>";
-        //     echo "<p>가격:".$product['price']."원</p>";
-        //     echo "<a href='page/product.php?id=".$product['id']."'>상세보기</a>";
-        //     echo "</div>";
-
-        // } -> 이것 지워야함! 
-
+        //products 에 들어가느건 상품 목록 id 는 뭐고 가격은 뭐고 암튼 그런거 
         
     ?>
 
     <?php if(count($products) === 0) : ?>
+        <!-- 행 갯수를 세는 명령어  -->
         <p>등록된 상품이 없습니다.</p>
     <?php else :  ?>
        <?php foreach($products as $product) :  ?>
         <!-- 받아온 배열을 foreach에서 출력한 것임 -->
+        <!-- 각각의 상품을 product 에 넣은 것임 product 는 상품의 이름을 나타냄  -->
         <h2><?= htmlspecialchars($product['name']) ?></h2>
         <p>가격 : <?= htmlspecialchars($product['price']) ?>원</p>
+        <!-- htmlspecialchars 는 < > & 같은 html 엔 티티? 같은걸로 바꿔서 보안을 예방함  -->
         <a href="page/product.php?id=<?=$product['id']?>">상세보기</a>
+        <!-- < ? =어쩌고 이 부분이 id 가 고유한 식별자가 되서 해당 상품을 구별할 수 있게 해줌 출력하는 것임  -->
     <?php endforeach; ?>
     <?php endif; ?>
 </body>
